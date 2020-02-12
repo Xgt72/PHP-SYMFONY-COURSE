3d968f3de0f862c022a33b97aa0db41eb5947eb0
Xavier <46873220+Xgt72@users.noreply.github.com>
2020-02-12 11:26:00 +0100

    solution for the quest 2-variables

---
+++ indy.php
@@ -0,0 +1,10 @@
+<?php
+$movieName = "Indiana Jones and the Last Crusade";
+$viewed = true;
+$releaseYear = 1989;
+$note = 8.2;
+echo '<p>Movie Name: ' . $movieName . '</p>' . PHP_EOL;
+echo '<p>Viewed: ' . $viewed .'</p>' . PHP_EOL;
+echo '<p>Year of release: ' . $releaseYear . '</p>' . PHP_EOL;
+echo '<p>IMDb note: ' . $note . '</p>' . PHP_EOL;
+?>
