80b4bb8d60635330a99b9971b3b9fdc6b2e6db97
Xavier <46873220+Xgt72@users.noreply.github.com>
2020-02-13 16:07:37 +0100

    Solution for quest 7
    Debug toi même

---
+++ index-debug.php
@@ -0,0 +1,21 @@
+<?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Xavier" => [
+        "city" => "La Loupe",
+        "weapon" => "Shirasaya Samurai Katana"
+    ]
+];
+
+var_dump($characters);
