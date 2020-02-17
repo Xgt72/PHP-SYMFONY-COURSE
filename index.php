85743915eff789568f39ce313b5708396aedd531
Xavier <46873220+Xgt72@users.noreply.github.com>
2020-02-17 09:44:00 +0100

    Solution for quest PDO
    PDO, un ami pour la vie.

--- index.php
+++ index.php
@@ -1,9 +1,44 @@
 <?php
-function writeSecretSentence(string $firstAnimal, string $secondAnimal): string
-{
-    $secretSentence = $firstAnimal . " s'incline face à " . $secondAnimal;
-    return $secretSentence;
+require_once 'connect.php';
+
+
+$pdo = new PDO(DSN, USER, PASS);
+
+echo getFriendsList($pdo);
+
+require 'form.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["firstname"])) {
+        $firstnameErr = "Firstname is required";
+    } else {
+        $firstname = test_input($_POST["firstname"]);
+    }
+
+    if (empty($_POST["lastname"])) {
+        $lastnameErr = "Lastname is required";
+    } else {
+        $lastname = test_input($_POST["lastname"]);
+    }
+
+    if ($firstnameErr == "" && $lastnameErr == "") {
+        $queryAddFriend = "INSERT INTO friend (firstname, lastname) VALUES (?, ?)";
+        $statementAddFriend = $pdo->prepare($queryAddFriend);
+        $statementAddFriend->execute([$firstname, $lastname]);
+        $location = "/index.php";
+        header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
+    }
 }
 
-echo writeSecretSentence('lion', 'lune');
-echo writeSecretSentence('loup', 'feu');
+function getFriendsList($pdo): string
+{
+    $friendList = "<h2>All the friends: </h2>\n<ul>";
+    $queryGetAllFriends = "SELECT * FROM friend";
+    $statementGetAllFriends = $pdo->query($queryGetAllFriends);
+    $friends = $statementGetAllFriends->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($friends as $friend) {
+        $friendList .= "<li>" . $friend['firstname'] . ' ' . $friend['lastname'] . "</li>";
+    }
+    $friendList .= "</ul>";
+    return $friendList;
+}
